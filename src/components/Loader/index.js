e968fad52b66b7b91ed5e417dbc90f2d66698852
Keari Eggers <kungfu.keari@gmail.com>
2017-07-03 14:49:01 -0500

    update loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
 
-const Loader = props => <div className="Loader" {...props}>Loading...</div>;
+const Loader = props => <div className="Loader" {...props}>Getting weather data...</div>;
 
 export default Loader;
