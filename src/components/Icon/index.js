75442ff968a3bb9b6b702835787bae083f52d2b1
Keari Eggers <kungfu.keari@gmail.com>
2017-07-20 13:18:31 -0500

    eject from create-react-app

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Icon = ({ icon,  ...props }) => {
-  const svg = require(`!raw-loader!./icons/${icon}.svg`);
+  const svg = require(`./icons/${icon}.svg`);
   return <span {...props} dangerouslySetInnerHTML={{__html: svg}}/>;
 };
 
